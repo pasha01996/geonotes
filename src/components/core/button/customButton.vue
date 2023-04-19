de839e9cc27cd5772f606a8ace455cfebc06ecca
Pavel Vahovich <92326230+pasha01996@users.noreply.github.com>
2023-04-19 15:43:33 -0700

    feat: add in core custom button

---
+++ src/components/core/button/customButton.vue
@@ -0,0 +1,36 @@
+<template>
+    <button
+    class="btn btn-primary d-flex align-items-center custom-button"
+    :class="extraClass"
+    >
+        <img
+            class="custom-icon"
+            :src="icon"
+            alt="icon"
+        />
+    </button>
+</template>
+
+<script setup>
+const props = defineProps({
+    icon: {
+        type: String,
+        default: '',
+    },
+    extraClass: {
+        type: String,
+        default: '',
+    }
+})
+</script>
+
+<style lang="sass">
+    .custom-button
+        padding: 10px
+        width: 45px
+        height: 45px
+    .custom-icon
+        color: #f2f5f7
+        width: 100%
+        height: 100%
+</style>
