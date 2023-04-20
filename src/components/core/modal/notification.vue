5ce23aa9ccbce64d2ff5ad22e892e4217da0e412
Pavel Vahovich <92326230+pasha01996@users.noreply.github.com>
2023-04-20 08:53:03 -0700

    feat: created a notification component

---
+++ src/components/core/modal/notification.vue
@@ -0,0 +1,50 @@
+<template>
+    <div v-if="show" class="notification">{{ message }}</div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+
+const props = defineProps({
+    message: {
+        type: String,
+        default: "You note added!"
+    },
+});
+
+const show = ref(true)
+
+const hideNotification = () => {
+    show.value = false
+}
+
+onMounted(() => {
+    show.value = true
+    setTimeout(() => {
+        hideNotification()
+    }, 1000)
+})
+</script>
+
+<style scoped>
+.notification {
+    position: fixed;
+    bottom: 20px;
+    left: 50%;
+    transform: translateX(-50%);
+    background-color: #fff;
+    padding: 10px;
+    border-radius: 5px;
+    box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.2);
+    animation: fade-in 0.5s ease-in-out;
+}
+
+@keyframes fade-in {
+    0% {
+        opacity: 0;
+    }
+    100% {
+        opacity: 1;
+    }
+}
+</style>
