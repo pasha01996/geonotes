845122af5baa2221c56ebdf0568493b1b6931f62
Pavel Vahovich <92326230+pasha01996@users.noreply.github.com>
2023-04-20 09:05:40 -0700

    feat: create form for adding note

---
+++ src/components/NotesTable/addNewNote.vue
@@ -0,0 +1,41 @@
+<template>
+    <h5 class="title_add_note">Write your note:</h5>
+    <textarea v-model="modalNoteText" type="text" class="form-control textarea"/>
+    <div class="dialog__footer">
+        <button type="button" class="btn btn-success" @click="onClickButton">Add note</button>
+    </div>
+</template>
+
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import Modal from '../core/modal/modal.vue'
+
+const modalNoteText = ref('')
+const emits = defineEmits(['onClickAddNote'])
+
+const onClickButton = () => {
+    emits('onClickAddNote', modalNoteText.value)
+    modalNoteText.value = ''
+}
+</script>
+
+
+<style lang="sass">
+.title_add_note
+    text-align: center
+    margin-bottom: 0
+    line-height: 1
+.textarea 
+    height: 200px
+    width: 300px
+
+.dialog__footer 
+    display: flex
+    justify-content: center
+
+@media (max-width: 700px)
+    .textarea
+        height: 180px
+        width: 250px
+</style>
